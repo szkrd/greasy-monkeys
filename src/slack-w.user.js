718be1ec0af9b8b0ffe946a579fea99ee5688b11
szkrd <szabolcs.kurdi@gmail.com>
2018-01-26 11:36:22 +0100

    feat(slack): hide add apps button in sidebar

--- src/slack-w.user.js
+++ src/slack-w.user.js
@@ -14,6 +14,15 @@
   // color scheme for copy-pasting:
   // `#343434,#242424,#c8427e,#fff2fa,#242424,#FFFFFF,#d74da5,#DE4C0D`
 
+  function loop () {
+    let sbLinkButtons = Array.from(document.querySelectorAll('.p-channel_sidebar__link'));
+    sbLinkButtons.forEach(el => {
+      if (el.textContent === '+ Add Apps') {
+        el.classList.add('add-apps-button');
+      }
+    });
+  }
+
   let css = `
     // reactions
     .rxn_panel, div.c-reaction_bar { display: none; }
@@ -31,7 +40,11 @@
     // bloated header
     #client_header { flex-basis: 5px; overflow: hidden; }
     #client_header:hover { flex-basis: 53px; }
+
+    // add apps ad button
+    .add-apps-button { display: none; }
   `;
 
   GM_addStyle(css.replace(/\/\/ .*/g, ''));
+  setInterval(loop, 1000);
 })();
