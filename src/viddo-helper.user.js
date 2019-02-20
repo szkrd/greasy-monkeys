03aad630d563b044ac92f3055ee6bca1ca60f389
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2019-02-20 20:40:12 +0100

    vdo loggers

--- src/viddo-helper.user.js
+++ src/viddo-helper.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         viddo-helper
+// @name         Viddo-helper
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  fill forms and stuff
@@ -14,26 +14,12 @@
     const $ = window.jQuery;
     const css = `
 #monkey-menu { width: 15px; height: 15px; position: fixed; z-index: 9999; border-radius: 10px; background: rgba(255,255,255,.2); opacity: .5; top: 3px; left: 3px; overflow: hidden; border: 1px solid gray; transition: all .2s linear; }
-#monkey-menu:hover { width: auto; height: auto; opacity: 1; border-radius: 0; background: orange; }
-#monkey-menu:hover div { display: block; }
-#monkey-menu div { font-weight: bold; cursor: pointer; padding: 2px 5px; display: none; }
-#monkey-menu div:hover { text-decoration: underline; }
+#monkey-menu:hover { width: auto; height: auto; opacity: 1; border-radius: 2px; background: orange; box-shadow: 2px 2px 2px rgba(0, 0, 0, .3); }
+#monkey-menu:hover li { display: block; }
+#monkey-menu li { font-weight: bold; cursor: pointer; padding: 2px 5px; display: none; }
+#monkey-menu li.action:hover { text-decoration: underline; }
 `;
     GM_addStyle(css.replace(/\/\/ .*/g, ''));
-
-    const moMenu = $('<div id="monkey-menu"></div>');
-    moMenu.prependTo(document.body);
-
-    function addMenuItem (title, action, flush) {
-        if (flush) {
-            moMenu.html('');
-        }
-        const menuItem = $(`<div>${title}</div>`);
-        if (action) {
-            menuItem.on('click', action);
-        }
-        menuItem.appendTo(moMenu);
-    }
 
     function fillInput (el, value) {
         el = $(el).get(0);
@@ -45,14 +31,27 @@
         el.dispatchEvent(new Event('input', { bubbles: true }));
     }
 
+    function addMenuItem (target, title, action, flush) {
+        if (flush) {
+            target.html('');
+        }
+        const menuItem = $(`<li>${title}</li>`);
+        if (action) {
+            menuItem.on('click', action);
+            menuItem.addClass('action');
+        }
+        menuItem.appendTo(target);
+    }
+
     let lastPath = '';
-    function updateMenu () {
+    function updatePathMenu () {
         const pathName = window.location.pathname;
         if (lastPath === pathName) {
             return;
         }
         lastPath = pathName;
-        addMenuItem('logout', () => { window.location.href = '/logout'; }, true);
+        const pathList = $('#monkey-menu-path');
+        pathList.html('');
         if (pathName === '/') {
             const login = (username, password, noSubmit) => {
                 fillInput($('.input.input--text:eq(0) input[type=text]'), username);
@@ -61,11 +60,11 @@
                     $('.button.button--orange:eq(0)').click();
                 }
             };
-            addMenuItem('login moderator', () => { login('moderatoruser'); });
-            addMenuItem('login uploader', () => { login('uploaderuser'); });
-            addMenuItem('login finance', () => { login('financeuser'); });
-            addMenuItem('login exporter', () => { login('exporteruser'); });
-            addMenuItem('login admin', () => { login('adminuser'); });
+            addMenuItem(pathList, 'login moderator', () => { login('moderatoruser'); });
+            addMenuItem(pathList, 'login uploader', () => { login('uploaderuser'); });
+            addMenuItem(pathList, 'login finance', () => { login('financeuser'); });
+            addMenuItem(pathList, 'login exporter', () => { login('exporteruser'); });
+            addMenuItem(pathList, 'login admin', () => { login('adminuser'); });
         }
         if (pathName === '/registration') {
             const reg = (username) => {
@@ -74,14 +73,53 @@
                 fillInput('input:eq(2)', '123456');
                 $('.radio-input__control--checkbox:eq(0)').click();
             };
-            addMenuItem('reg alma', () => { reg('alma'); });
-            addMenuItem('reg korte', () => { reg('korte'); });
-            addMenuItem('reg eper', () => { reg('eper'); });
-            addMenuItem('reg dinnye', () => { reg('dinnye'); });
+            addMenuItem(pathList, 'reg alma', () => { reg('alma'); });
+            addMenuItem(pathList, 'reg korte', () => { reg('korte'); });
+            addMenuItem(pathList, 'reg eper', () => { reg('eper'); });
+            addMenuItem(pathList, 'reg dinnye', () => { reg('dinnye'); });
         }
+    }
+
+    function createMenu () {
+        const moMenu = $('<div id="monkey-menu"><ul id="monkey-menu-path"></ul><ul id="monkey-menu-static"></ul></div>');
+        moMenu.prependTo(document.body);
+        return moMenu;
+    }
+
+    function addStorageSwitcherMenuItem (target, name, value) {
+        const key = `_${name}`;
+        const action = () => {
+            const thisMenuItem = $(`#monkey-menu-switcher-${name}`);
+            const current = localStorage.getItem(key);
+            if (current) {
+                thisMenuItem.text(thisMenuItem.text().replace('☑', '☐'));
+                localStorage.removeItem(key);
+            } else {
+                thisMenuItem.text(thisMenuItem.text().replace('☐', '☑'));
+                localStorage.setItem(key, value);
+            }
+        };
+        const current = localStorage.getItem(key);
+        const title = (current ? '☑' : '☐') + ` ls ${name}`;
+        const menuItem = $(`<li id="monkey-menu-switcher-${name}">${title}</li>`);
+        menuItem.on('click', action);
+        menuItem.addClass('action');
+        menuItem.appendTo(target);
+        return menuItem;
     }
 
     // ---
 
-    moMenu.on('mouseenter', updateMenu);
+    (() => {
+        const menu = createMenu();
+        const staticList = $('#monkey-menu-static');
+        addMenuItem(staticList, 'logout', () => { window.location.href = '/logout'; });
+        addStorageSwitcherMenuItem(staticList, 'logRedux', '1');
+        addStorageSwitcherMenuItem(staticList, 'logHlsjs', '1');
+        addStorageSwitcherMenuItem(staticList, 'logAxios', '1');
+        addStorageSwitcherMenuItem(staticList, 'logSocketio', '1');
+        addStorageSwitcherMenuItem(staticList, 'logTimeStamp', '1');
+        addStorageSwitcherMenuItem(staticList, 'logLevel', '0');
+        menu.on('mouseenter', updatePathMenu);
+    })();
 })();
