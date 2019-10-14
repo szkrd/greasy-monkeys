0ba68782243558b9b1da1c4084f7965f46aa666c
Szabolcs Kurdi <szabolcs.kurdi@planorama.com>
2019-10-14 13:02:29 +0200

    add portal menu tool

--- src/portal.user.js
+++ src/portal.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Portal-helper
 // @namespace    http://tampermonkey.net/
-// @version      1.0.3
-// @description  tries to detect react component chains (named functions and classes only)
+// @version      2.0.0
+// @description  helper functions for portal
 // @author       szkrd
 // @match        http://localhost:3001/*
 // @require      https://code.jquery.com/jquery-3.2.0.slim.min.js
@@ -14,6 +14,13 @@
     const WAIT_TIME = 800;
     const $ = window.jQuery;
     const css = `
+#monkey-menu { width: 15px; height: 15px; position: fixed; z-index: 9999; border-radius: 10px; background: rgba(255,255,255,.2); opacity: .5; top: 3px; left: 3px; overflow: hidden; border: 1px solid gray; transition: all .2s linear; padding: 3px; }
+#monkey-menu:hover { width: auto; height: auto; opacity: 1; border-radius: 2px; background: cyan; box-shadow: 2px 2px 2px rgba(0, 0, 0, .3); }
+#monkey-menu:hover li { display: block; }
+#monkey-menu > ul { padding: 0; margin: 0; font-size: 11px; font-weight: normal; }
+#monkey-menu li { font-weight: bold; cursor: pointer; padding: 2px 5px; display: none; }
+#monkey-menu li.action:hover { text-decoration: underline; }
+
 #monkey-react-path { display: none; z-index: 9999; font-family: Arial; font-size: 12px; position: fixed; background: #f2f2f2; padding: 1px 3px; border-top: 1px solid #cfcfcf; border-left: 1px solid #cfcfcf; border-radius: 3px 0 0 0; bottom: 0; right: 0; }
 #monkey-react-path a { cursor: pointer; padding: 2px 3px; display: inline-block; }
 #monkey-react-path a.hidden { color: #aaa; font-size: 10px; }
@@ -161,7 +168,7 @@
                 if (tree.length) {
                     const html = tree.map(meta => {
                         const className = (meta.show ? 'visible' : 'hidden') + (meta.confusing ? ' confusing' : '');
-                        return `<a class="${className}">${meta.name}</a>`
+                        return `<a class="${className}">${meta.name}</a>`;
                     }).join(' ↘ ');
                     moReactPath.html(html);
                 }
@@ -170,11 +177,61 @@
         return moReactPath;
     }
 
+    // ---
+
+    function createMenu () {
+        const moMenu = $('<div id="monkey-menu"><ul id="monkey-menu-path"></ul><ul id="monkey-menu-static"></ul></div>');
+        moMenu.prependTo(document.body);
+        return moMenu;
+    }
+
+    function addMenuItem (target, title, action, flush) {
+        if (flush) {
+            target.html('');
+        }
+        const menuItem = $(`<li>${title}</li>`);
+        if (action) {
+            menuItem.on('click', action);
+            menuItem.addClass('action');
+        }
+        menuItem.appendTo(target);
+    }
+
+    function addStorageSwitcherMenuItem (target, name, text, value, actionTrue, actionFalse) {
+        const key = `_${name}`;
+        const action = () => {
+            const thisMenuItem = $(`#monkey-menu-switcher-${name}`);
+            const current = localStorage.getItem(key);
+            if (current) {
+                thisMenuItem.text(thisMenuItem.text().replace('☑', '☐'));
+                localStorage.removeItem(key);
+                if (actionFalse) { actionFalse(); }
+            } else {
+                thisMenuItem.text(thisMenuItem.text().replace('☐', '☑'));
+                localStorage.setItem(key, value);
+                if (actionTrue) { actionTrue(); }
+            }
+        };
+        const current = localStorage.getItem(key);
+        const title = (current ? '☑' : '☐') + ' ' + text;
+        const menuItem = $(`<li id="monkey-menu-switcher-${name}">${title}</li>`);
+        menuItem.on('click', action);
+        menuItem.addClass('action');
+        menuItem.appendTo(target);
+        return menuItem;
+    }
+
     // run
     // ===
 
     // Startup ↘ App ↘ Layout ↘ Checks ↘ Filter ↘ SwitchWrapper ↘ ReactSwitch
     (() => {
+        createMenu();
+        const staticList = $('#monkey-menu-static');
+        addMenuItem(staticList, 'remove locale ls data', () => { localStorage.removeItem('locale'); });
+        addStorageSwitcherMenuItem(staticList, 'logRedux', 'use redux-logger', '1');
+        addStorageSwitcherMenuItem(staticList, 'logTimeStamp', 'log with timestamps', '1');
+        addStorageSwitcherMenuItem(staticList, 'logLevel', 'log verbosity all', '0');
         createReactPathLogger();
     })();
 })();
