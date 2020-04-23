e011766af6783e567f8b7754316558612a52b576
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2020-04-23 20:29:43 +0200

    new slack design support

--- src/slack-app.user.js
+++ src/slack-app.user.js
@@ -1,44 +1,52 @@
 // ==UserScript==
 // @name         Slack condensed
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
-// @description  Mostly decrease list sizes in the sidebar
+// @version      2.0.0
+// @description  Mostly decrease list sizes in the sidebar and grey out less important items
 // @author       szkrd
 // @match        https://app.slack.com/*
 // @require      https://code.jquery.com/jquery-3.2.0.slim.min.js
 // @grant        GM_addStyle
 // ==/UserScript==
-
 (function () {
     'use strict';
-    // const $ = window.jQuery;
+    const $ = window.jQuery;
+    const updateInterval = 1000;
+    let isActive = false;
     let css = `
-// mute avatars
-.member_image, img.c-avatar__image, div.message_icon { filter: grayscale(100%); }
+//# jira message boxes
+//# ------------------
+body.GM_active div.GM_list-item--jira-cloud .c-message_kit__blocks b,
+body.GM_active div.GM_list-item--jira-cloud a[data-qa="message_sender_name"],
+body.GM_active div.GM_list-item--jira-cloud .c-message_kit__gutter__left img.c-avatar__image,
+body.GM_active div.GM_list-item--jira-cloud div.p-section_block__accessory
+{ opacity: .5; color: red !important; filter: grayscale(100%); }
 
-// spacers in the sidebar
-div.p-channel_sidebar__static_list div[role=presentation]:empty { display: none; }
-
-// ctrl-k "searchbar" nonsense in the sidebar
-div.p-channel_sidebar__navigation_bar { background: #fff; margin-top: 0; margin-bottom: 0; height: 6px; border-radius: 3px; opacity: .2; position: relative; top: -3px; }
-div.p-channel_sidebar__navigation_bar:hover { opacity: .4; }
-
-// condense list items in the sidebar
-div.p-channel_sidebar__static_list .c-virtual_list__item { position: static; }
-div.p-channel_sidebar__static_list div[role=listitem] { height: 20px !important; font-size: 13px; }
-div.p-channel_sidebar__static_list div[role=listitem] a,
-div.p-channel_sidebar__static_list div[role=listitem] button,
-div.p-channel_sidebar__static_list div[role=listitem] > div.p-channel_sidebar__section_heading { line-height: 20px !important; height: 20px !important; }
-
-// condensed subsection headings: pull left
-div.p-channel_sidebar__static_list div[role=listitem] button { opacity: .6; margin-left: -10px; }
-
-// sidebar list item small X buttons
-div.p-channel_sidebar__static_list .p-channel_sidebar__close { zoom: .8; }
-div.p-channel_sidebar__static_list .p-channel_sidebar__close > i { top: -4px; left: -3px; position: relative; }
-
-#monkey_textArea { position: fixed; bottom: 26px; right: 90px; z-index: 200; width: 50%; opacity: .8; border-radius: 3px; font-size: 13px; height: 38px;}
+//# sidebar
+//# -------
+body.GM_active .p-channel_sidebar__static_list__item { position: static!!; }
+body.GM_active .p-channel_sidebar__channel,
+body.GM_active .p-channel_sidebar__link { height: auto!!; line-height: 1!!; font-size: 14px; }
 `;
+    css = css.replace(/!!/g, ' !important').replace(/\/\/#.*/g, '');
+    GM_addStyle(css);
 
-    GM_addStyle(css.replace(/\/\/ .*/g, ''));
+    // hitting F2 (outside a slack managed textfield) toggles the changes
+    $(document.body).on('keyup', e => {
+        if (e.key === 'F2') { isActive = !isActive; $(e.target).toggleClass('GM_active', isActive); }
+    });
+
+    setInterval(() => {
+        if (!isActive) return;
+
+        // if the element has the app id of Jira Cloud
+        // or it has text with jira-like strings...
+        $('[data-stringify-text="Jira Cloud"]').closest('div[role="listitem"]').addClass('GM_list-item--jira-cloud');
+        $('div.p-mrkdwn_element > span > b').each((i, el) => {
+            const $el = $(el);
+            if (/(transitioned a|⟶)/.test($el.text())) {
+                $el.closest('div[role="listitem"]').addClass('GM_list-item--jira-cloud');
+            }
+        });
+    }, updateInterval);
 })();
