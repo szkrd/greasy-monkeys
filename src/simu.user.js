8f9082b5449804b03fc35f0de7c18535de347127
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2021-09-09 14:32:32 +0200

    fluent modal related fixes

--- src/simu.user.js
+++ src/simu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Simu
 // @namespace    http://tampermonkey.net/
-// @version      2.4.1
+// @version      2.4.5
 // @description  Simulator helper.
 // @author       szkrd
 // @match        http://localhost:3000/*
@@ -11,7 +11,7 @@
 (function () {
     'use strict';
     // it can run on multiple ports, if that's what we want
-    if (![3000].includes(~~window.location.port)) return;
+    if (~~window.location.port < 3000) return;
 
     const SOURCE_WITH_LINE_COL = true; // should copy to clipboard use file path with line & col
     const WAIT_TIME = 800; // mouse hover timeout
@@ -23,13 +23,14 @@
 #monkey-menu:hover { width: auto; height: auto; opacity: 1; border-radius: 2px; background: cyan; box-shadow: 2px 2px 2px rgba(0, 0, 0, .3); }
 #monkey-menu:hover li { display: block; }
 #monkey-menu > ul { padding: 0; margin: 0; font-size: 11px; font-weight: normal; }
-#monkey-menu li { font-weight: bold; cursor: pointer; padding: 2px 5px; display: none; }
+#monkey-menu li { font-weight: bold; cursor: pointer; padding: 2px 5px; display: none; color: #000; }
 #monkey-menu li.action:hover { text-decoration: underline; }
 
 html.monkey-react-path-disabled #monkey-react-path { display: none; }
-#monkey-react-path { z-index: 1000001; font-family: Arial; font-size: 12px; position: fixed; background: #f2f2f2; padding: 1px 3px; border-top: 1px solid #cfcfcf; border-left: 1px solid #cfcfcf; border-radius: 3px 0 0 0; bottom: 0; right: 0; }
-#monkey-react-path a { cursor: pointer; padding: 2px 3px; display: inline-block; }
-#monkey-react-path a.without-source { opacity: .5; }
+#monkey-react-path { opacity: .7; color: #000; z-index: 1000001; font-family: Arial; font-size: 12px; position: fixed; background: #f2f2f2; padding: 1px 3px; border-top: 1px solid #cfcfcf; border-left: 1px solid #cfcfcf; border-radius: 3px 0 0 0; bottom: 0; right: 0; }
+#monkey-react-path:hover { opacity: 1; }
+#monkey-react-path a { cursor: pointer; padding: 2px 3px; display: inline-block; color: #000; }
+#monkey-react-path a.without-source { opacity: .4; }
 #monkey-react-path a.hidden { color: #aaa; font-size: 10px; }
 #monkey-react-path a.hidden.confusing { color: #555; }
 #monkey-react-path a:hover { text-decoration: underline; }
@@ -37,6 +38,7 @@ html.monkey-react-path-disabled #monkey-react-path { display: none; }
     GM_addStyle(css.replace(/\/\/ .*/g, ''));
 
     // material UI v3 component names not active for now, see portal script for details
+    // (should I need a fluent detector, they use /^ms-/ for classNames)
     const matUiV3CompNames = [];
     const hasMuiClass = node => Array.from((node || {}).classList || []).some(className => className.startsWith('Mui'));
     const otherPackages = BLACKLISTED_COMPONENT_NAMES.filter(item => typeof item === 'string');
@@ -134,6 +136,7 @@ html.monkey-react-path-disabled #monkey-react-path { display: none; }
         return metas;
     }
 
+    let reactPathMouseMoveHandler;
     function createReactPathLogger () {
         const moReactPath = $('<div id="monkey-react-path">&nbsp;</div>');
         moReactPath.prependTo(document.body);
@@ -157,7 +160,7 @@ html.monkey-react-path-disabled #monkey-react-path { display: none; }
         });
         moReactPath.show();
         let throttleTimer;
-        $(document).on('mousemove', (e) => {
+        const mouseMoveHandler = reactPathMouseMoveHandler = (e) => {
             clearTimeout(throttleTimer);
             throttleTimer = null;
             throttleTimer = setTimeout(() => {
@@ -165,15 +168,22 @@ html.monkey-react-path-disabled #monkey-react-path { display: none; }
                 if (tree.length) {
                     const html = tree.map(meta => {
                         let className = (meta.show ? 'visible' : 'hidden') + (meta.confusing ? ' confusing' : '');
-                        if (!meta.source) className += ' without-source';
+                        let title = '';
+                        if (!meta.source) {
+                            className += ' without-source';
+                        } else {
+                            title = meta.source.replace(/\\/g, '/');
+                            if (title.includes('/src/')) title = title.replace(/^.*\/src\//, '/');
+                        }
                         const source = escapeHtml(meta.source);
                         const name = escapeHtml(meta.name);
-                        return `<a class="${className}" data-source="${source}">${name}</a>`;
+                        return `<a class="${className}" data-source="${source}" title="${title}">${name}</a>`;
                     }).join(' ↘ ');
                     moReactPath.html(html);
                 }
             }, WAIT_TIME);
-        });
+        };
+        $(document).on('mousemove', mouseMoveHandler);
         return moReactPath;
     }
 
@@ -241,12 +251,25 @@ html.monkey-react-path-disabled #monkey-react-path { display: none; }
         }
 
         const staticList = $('#monkey-menu-static');
-        // addMenuItem(staticList, 'remove locale ls data', () => { localStorage.removeItem('locale'); });
         addMenuItem(staticList, 'toggle react path', menuItemToggleReactPath);
+        addStorageSwitcherMenuItem(staticList, 'logHistory', 'log history change', '1');
         addStorageSwitcherMenuItem(staticList, 'logRedux', 'log redux actions', '1');
+        addStorageSwitcherMenuItem(staticList, 'logFetch', 'log fetch', '1');
         addStorageSwitcherMenuItem(staticList, 'logWebSocket', 'log websocket', '1');
         addStorageSwitcherMenuItem(staticList, 'logTimeStamp', 'log with timestamps', '1');
         addStorageSwitcherMenuItem(staticList, 'logLevel', 'log level to zero', '0');
         createReactPathLogger();
+
+        // fluent modals stop the bubbling of various events, so mousemove would not reach our topmost handler
+        if (reactPathMouseMoveHandler) {
+            addMenuItem(staticList, 'attach to fluent modal', () => {
+                const marker = 'monkeyMouseMoveHack';
+                const wrapper = $(`div.ms-Fabric.ms-Layer-content > div[aria-modal="true"]:not(.${marker})`);
+                if (wrapper.length > 0) {
+                    wrapper.parent().on('mousemove', reactPathMouseMoveHandler);
+                    wrapper.addClass(marker);
+                }
+            });
+        }
     })();
 })();
